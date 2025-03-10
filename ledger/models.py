56a9e62934eb19c0998a43f8de7152472b2566b6
robmalonzo <robastianmalonzo@gmail.com>
2025-03-10 23:25:34 +0800

    modified models

--- ledger/models.py
+++ ledger/models.py
@@ -1,30 +1,42 @@
 from django.db import models
+from django.contrib.auth.models import User
 from django.urls import reverse
 
 # Create your models here.
 
+
 class Ingredient(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('ledger:ingredient-detail', args=[(self.pk)])
+        return reverse('ledger:ingredient_detail', args=[str(self.pk)])
+
 
 class Recipe(models.Model):
-    name = models.CharField(max_length=255, unique=True)
+    name = models.CharField(max_length=100)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, null=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.name
 
     def get_absolute_url(self):
-        return reverse('ledger:recipe-detail', args=[(self.pk)])
+        return reverse('ledger:recipe_detail', args=[str(self.pk)])
+
 
 class RecipeIngredient(models.Model):
-    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE, related_name="ingredients")
-    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE, related_name="recipe")
     quantity = models.CharField(max_length=100)
-
-
-   
+    ingredient_field = models.ForeignKey(
+        Ingredient,
+        on_delete=models.CASCADE,
+        related_name='recipe'
+    )
+    recipe_field = models.ForeignKey(
+        Recipe,
+        on_delete=models.CASCADE,
+        related_name='ingredients'
+    )
